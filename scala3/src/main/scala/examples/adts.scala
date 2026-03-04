7ed5d1a572e16aeacf50a9ef93941bb2c7e4959f
dancewithheart <dancingwithopenheart@gmail.com>
2026-03-04 23:32:14 +0100

    polymorhic data types - output Scala3 files

--- scala3/src/main/scala/examples/adts.scala
+++ scala3/src/main/scala/examples/adts.scala
@@ -27,3 +27,11 @@ object adts:
   def withEscapes(): String = "line1\nline2\t\"quote\"\\backslash"
 
   def id[A](x1: A): A = x1
+
+  enum Maybe[A]:
+    case Just[A](x0: A) extends Maybe[A]
+    case None extends Maybe[Nothing]
+
+  enum List[X]:
+    case Nil extends List[Nothing]
+    case Cons[X](x0: X, x1: List[X]) extends List[X]
