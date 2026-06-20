ddde71944eb92c3153d3adea8d146d5ae4e90d24
Piotr Paradzinski <dancingwithopenheart@gmail.com>
2026-06-20 20:37:07 +0200

    test HedgehogMain => Props

--- test/Props.hs
+++ test/Props.hs
@@ -1,21 +1,21 @@
 module Main where
 
+import Hedgehog (Group, checkParallel)
 import qualified CompileProps
 import qualified Compile.TermsProps
 import qualified Compile.TypesProps
-import Hedgehog (Group, checkParallel)
-import qualified Name.NameEnvProps
+import Lower.IRToScalaProps (iRToScalaProps)
+import Name.NameEnvProps (nameEnvProps)
 import qualified Name.NamePolicyProps
 import qualified Render.PrintProps
-import qualified IRToScalaProps
 
 main :: IO ()
 main = do
-    ok1 <- checkParallel Name.NameEnvProps.tests
+    ok1 <- checkParallel nameEnvProps
     ok2 <- checkParallel Render.PrintProps.tests
     ok3 <- checkParallel CompileProps.tests
-    ok4 <- checkGroups Name.NamePolicyProps.tests
-    ok5 <- checkParallel IRToScalaProps.tests
+    ok4 <- checkParallel iRToScalaProps
+    ok5 <- checkGroups Name.NamePolicyProps.tests
     ok6 <- checkParallel Compile.TermsProps.tests
     ok7 <- checkParallel Compile.TypesProps.tests
     if ok1 && ok2 && ok3 && ok4 && ok5 && ok6 && ok7 then pure () else fail "Hedgehog tests failed"
